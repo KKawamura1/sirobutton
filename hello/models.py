819852130cceb30e9edf8e03cdbb159126eb7b9b
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-19 15:37:03 +0900

    add models

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
-# Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+
+class Video(models.Model):
+    video_id = models.CharField('youtube video id', max_length=20)
+    title = models.CharField('video title', max_length=200)
+
+
+class CaptionTrack(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    caption_id = models.CharField('youtube caption track id', max_length=20)
+    language = models.CharField('caption language', max_length=20)
+
+
+class Subtitle(models.Model):
+    captiontrack = models.ForeignKey(CaptionTrack, on_delete=models.CASCADE)
+    begin = models.TimeField('captions begin')
+    end = models.TimeField('captions end')
+    content = models.CharField('caption content', max_length=500)
