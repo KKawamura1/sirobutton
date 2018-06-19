e4f4be134b3e11430e84c62da79be4108fed0ae4
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-19 20:10:16 +0900

    add postgresql

--- sirobutton/settings.py
+++ sirobutton/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -82,6 +83,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'].update(dj_database_url.config(conn_max_age=400))
 
 
 # Password validation
