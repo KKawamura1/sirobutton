d486941ddf1e11320135c2bfa5d5b73edfd38cbe
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-21 13:10:28 +0900

    add some fields

--- hello/models.py
+++ hello/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+from hitcount.models import HitCount, HitCountMixin
 
 
 class Video(models.Model):
     video_id = models.CharField('youtube video id', max_length=20)
     title = models.CharField('video title', max_length=200)
+    last_updated = models.DateTimeField('last modified time', auto_now=True)
 
     def __str__(self) -> str:
         return '<Video: {}>'.format(self.title)
@@ -13,16 +16,23 @@ class CaptionTrack(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
     caption_id = models.CharField('youtube caption track id', max_length=20)
     language = models.CharField('caption language', max_length=20)
+    last_updated = models.DateTimeField('last modified time', auto_now=True)
 
     def __str__(self) -> str:
         return '<CaptionTrack of {}, language: {}>'.format(self.video, self.language)
 
 
-class Subtitle(models.Model):
+class Subtitle(models.Model, HitCountMixin):
     captiontrack = models.ForeignKey(CaptionTrack, on_delete=models.CASCADE)
     begin = models.TimeField('captions begin')
     end = models.TimeField('captions end')
     content = models.CharField('caption content', max_length=500)
+    short_title = models.CharField('short version of caption content', max_length=30)
+    yomi = models.CharField('hiragana yomi for the content', max_length=500)
+    last_updated = models.DateTimeField('last modified time', auto_now=True)
+    # see: http://django-hitcount.readthedocs.io/en/latest/installation.html#models
+    hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk',
+                                        related_query_name='hit_count_generic_relation')
 
     def __str__(self) -> str:
         return '<Subtitle: {}>'.format(self.content)
