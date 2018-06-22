81f8380f3f84cb7962251407946c8232a4e37744
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-22 10:00:08 +0900

    move the order

--- sirobutton/settings.py
+++ sirobutton/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello',
     'hitcount',
+    'hello',
 ]
 
 MIDDLEWARE = [
