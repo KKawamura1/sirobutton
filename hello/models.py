9e5ad6687de3e1b790dc86a8b2cc4d7549a16a97
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-22 11:01:52 +0900

    update model since its charactor is too short

--- hello/models.py
+++ hello/models.py
@@ -6,8 +6,8 @@ from hitcount.models import HitCount, HitCountMixin
 
 class Video(models.Model):
     id = models.AutoField(primary_key=True)
-    video_id = models.CharField('youtube video id', max_length=20)
-    title = models.CharField('video title', max_length=200)
+    video_id = models.CharField('youtube video id', max_length=50)
+    title = models.CharField('video title', max_length=500)
     last_updated = models.DateTimeField('last modified time', auto_now=True)
 
     def __str__(self) -> str:
@@ -17,7 +17,7 @@ class Video(models.Model):
 class CaptionTrack(models.Model):
     id = models.AutoField(primary_key=True)
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
-    caption_id = models.CharField('youtube caption track id', max_length=20)
+    caption_id = models.CharField('youtube caption track id', max_length=50)
     language = models.CharField('caption language', max_length=20)
     last_updated = models.DateTimeField('last modified time', auto_now=True)
 
@@ -31,7 +31,7 @@ class Subtitle(models.Model, HitCountMixin):
     begin = models.TimeField('captions begin')
     end = models.TimeField('captions end')
     content = models.CharField('caption content', max_length=500)
-    short_title = models.CharField('short version of caption content', max_length=30)
+    short_title = models.CharField('short version of caption content', max_length=50)
     yomi = models.CharField('hiragana yomi for the content', max_length=500)
     last_updated = models.DateTimeField('last modified time', auto_now=True)
     # see: http://django-hitcount.readthedocs.io/en/latest/installation.html#models
