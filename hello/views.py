db4a5e1e1fa5b2e247644abeeab7af429cf79b3f
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-24 12:42:17 +0900

    show page list

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.views import generic
-from typing import List
+from django.core.paginator import Paginator, EmptyPage, InvalidPage, PageNotAnInteger
+from pure_pagination.mixins import PaginationMixin
+from typing import List, Any, Dict
 
 from .models import Video, CaptionTrack, Subtitle
-
-# Create your views here.
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -20,3 +20,81 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return subtitles."""
         return Subtitle.objects.all()
+
+
+class SubtitleListView(generic.ListView, generic.list.MultipleObjectMixin):
+    model = Subtitle
+    context_object_name = 'subtitles'
+    template_name = 'subtitle_list.html'
+    paginate_by = 50
+    pages_around = 3
+    pages_edge = 2
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['pages'] = self._get_pages(context)
+        return context
+
+    def _get_pages(self, context: Dict[str, Any]) -> List[Dict[str, Any]]:
+        page_obj = context['page_obj']
+        page_number = page_obj.number
+        page_max = page_obj.paginator.num_pages
+
+        # [1, 2], [...], [7], [8], [9], [...] [14, 15]
+        # [edge] [elip] [around] [you_are_here] [around] [elip] [edge]
+
+        # left
+        rest_pages = page_number - 1
+        # left around
+        if rest_pages > 0:
+            num = min(SubtitleListView.pages_around, rest_pages)
+            rest_pages -= num
+            left_around_pages = [dict(type='around-left', number=(page_number-page-1), link=True)
+                                 for page in range(num)][::-1]
+        else:
+            left_around_pages = []
+        # left edge
+        if rest_pages > 0:
+            num = min(SubtitleListView.pages_edge, rest_pages)
+            rest_pages -= num
+            left_edge_pages = [dict(type='edge-left', number=(page+1), link=True)
+                               for page in range(num)]
+        else:
+            left_edge_pages = []
+        # left ellipsis
+        if rest_pages > 0:
+            left_ellip = [dict(type='ellip-left', link=False)]
+        else:
+            left_ellip = []
+
+        # right
+        rest_pages = page_max - page_number
+        # right around
+        if rest_pages > 0:
+            num = min(SubtitleListView.pages_around, rest_pages)
+            rest_pages -= num
+            right_around_pages = [dict(type='around-right', number=(page_number+page+1), link=True)
+                                  for page in range(num)]
+        else:
+            right_around_pages = []
+        # right edge
+        if rest_pages > 0:
+            num = min(SubtitleListView.pages_edge, rest_pages)
+            rest_pages -= num
+            right_edge_pages = [dict(type='edge-right', number=(page_max-page), link=True)
+                                for page in range(num)][::-1]
+        else:
+            right_edge_pages = []
+        # right ellipsis
+        if rest_pages > 0:
+            right_ellip = [dict(type='ellip-right', link=False)]
+        else:
+            right_ellip = []
+
+        # you_are_here
+        you_are_here = [dict(type='you_are_here', number=page_number, link=False)]
+
+        pages = (left_edge_pages + left_ellip + left_around_pages
+                 + you_are_here
+                 + right_around_pages + right_ellip + right_edge_pages)
+        return pages
