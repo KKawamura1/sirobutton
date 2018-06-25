698a5f1f919b0e9af92feef118f13c8c5f341553
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-25 13:22:59 +0900

    change url structures

--- sirobutton/urls.py
+++ sirobutton/urls.py
@@ -4,13 +4,7 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
 urlpatterns = [
-    url(r'^$', hello.views.SubtitleListView.as_view(), name='index'),
-    path('lists/', hello.views.SubtitleListView.as_view(), name='lists'),
-    path('subtitle/<int:pk>/', hello.views.SubtitleDetailView.as_view(), name='subtitle-detail'),
-    path('jump-to-youtube/<int:pk>/', hello.views.RedirectToYoutubeView.as_view(),
-         name='jump-to-youtube'),
+    path('sirobutton/', include('hello.urls')),
     path('admin/', admin.site.urls),
 ]
