767f6c75fd63b3f2b9c8577c2555134f48c10d38
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-25 22:41:20 +0900

    add tag migrations

--- hello/admin.py
+++ hello/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Video, CaptionTrack, Subtitle
+from .models import Video, CaptionTrack, Subtitle, Tag
 
 # register my models to change it on admin console
 
 admin.site.register(Video)
 admin.site.register(CaptionTrack)
 admin.site.register(Subtitle)
+admin.site.register(Tag)
