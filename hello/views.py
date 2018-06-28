5264a9b807bfb742d991fe6b24c8f39b536f4434
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-28 14:11:44 +0900

    modify oembed

--- hello/views.py
+++ hello/views.py
@@ -336,23 +336,23 @@ class OEmbedView(generic.View):
         #                                       static('hello/images/botan.png'))
         botan_url = 'https://{}{}'.format(request.get_host(),
                                           static('hello/images/botan.png'))
+        width = 540
+        height = 160
         html = f"""
-        <div style="width:100%; height:120px; border:1px solid; font-weight:bold; overflow:hidden; background-image:linear-gradient(to bottom, #dafaff 0%, #caeafa 100%);">
+        <div style="width:{width}px; height:{height}px; border:1px solid; font-weight:bold; overflow:hidden; background-image:linear-gradient(to bottom, #dfffff 0%, #caeafa 100%);">
           <div style="display:flex; margin-right: auto; margin-left: auto; justify-content: center; align-items: center;">
             <img src="{botan_url}" style="max-width: 36px; height: auto; margin-top: auto; margin-bottom: auto; vertical-align: middle; border-style: none;" alt="white botan">
-            <h1 style="font-size: 3.6em; margin-top: auto; margin-bottom: auto; text-shadow: 0px 0.5px 2px #cff">
+            <h1 style="font-size: 2.4em; margin-top: auto; margin-bottom: auto; text-shadow: 0px 0.5px 2px #cff">
               シロボタン
             </h1>
             <img src="{botan_url}" style="max-width: 36px; height: auto; margin-top: auto; margin-bottom: auto; vertical-align: middle; border-style: none;" alt="white botan">
           </div>
-          <hr style="margin-bottom: 1.5rem; margin-top: 1.5rem; border: 0; border-top: 1px solid rgba(0, 0, 0, 0.1); box-sizing: content-box; height: 0; overflow: visible;">
-          <p style="font-size: 1.25rem; font-weight: 300; margin-top: 0; margin-bottom: 1rem;">
+          <hr style="margin-bottom: 0.6rem; margin-top: 1.0rem; border: 0; border-top: 1px solid rgba(0, 0, 0, 0.1); box-sizing: content-box; height: 0; overflow: visible;">
+          <p style="font-size: 1.25rem; font-weight: 300; margin-top: 0; margin-bottom: 1rem; margin-left: 0.2rem; margin-right: 0.2rem;">
             <span style="text-shadow: 0px 0.5px 2px #cff">シロボタン</span>は、電脳少女シロちゃんの声を検索して再生できるWebアプリケーションです。
           </p>
         </div>
-        """.format(img_url=botan_url)
-        width = 480
-        height = 120
+        """.replace('\n', '').strip()
         data = dict(type=data_type, version=1.0, title=title,
                     author_name=author_name, author_url=author_url,
                     provider_name=author_name, provider_url=author_url,
@@ -372,7 +372,7 @@ class OEmbedView(generic.View):
               <provider_url>{author_url}</provider_url>
               <width>{width}</width>
               <height>{height}</height>
-              <html>{html}</html>
+              <html>{html.replace('<', '&lt;').replace('>', '&gt;')}</html>
             </oembed>
-            """
+            """.replace('\n', '').strip()
             return HttpResponse(xml_data, content_type="application/xml", charset="UTF-8")
