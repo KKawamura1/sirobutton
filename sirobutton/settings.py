ed175ff12c846426232286bcd91515a0dadc2889
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-07-03 20:00:46 +0900

    add sites framework to use ping_google()

--- sirobutton/settings.py
+++ sirobutton/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'django.contrib.sites',
     'hitcount',
     'pure_pagination',
     'hello',
